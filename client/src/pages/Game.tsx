2b3f370df886e4b16d165ed8d0a8f57eb60a3a62
Mamadou Saliou Dunn <salioudunn11@gmail.com>
2026-07-02 11:34:11 -0400

    Add files via upload

---
+++ client/src/pages/Game.tsx
@@ -0,0 +1,20 @@
+import{
+     IonButton, IonInput, IonHeader, IonTitle, IonToolbar, IonPage
+     } '@ionic/react'; 
+     
+     function game(){ 
+        return (
+            
+            <IonPage> 
+            <IonHeader> 
+
+            <IonToolbar>
+             <IonTitle> This is the game</IonTitle> 
+             </IonToolbar> 
+             
+             </IonHeader> 
+             </IonPage> 
+            
+            );
+         } 
+         export default game
