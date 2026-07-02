81b33725ce07927297346217d4b5004059e382a4
Mst Khatun <mitunureen1984@gmail.com>
2026-07-02 12:37:42 -0400

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,15 +1,95 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React from "react";
+import {IonButton,IonContent,IonHeader,IonInput,IonItem,IonLabel,IonPage,IonTitle,IonToolbar,} from "@ionic/react";
+
+import { Controller, useForm } from "react-hook-form";
+
+type CreateGameForm = {
+  roomCode: string;
+  celebrity: string;
+};
 
 const Home: React.FC = () => {
+  const { control, handleSubmit, reset } = useForm<CreateGameForm>({
+    defaultValues: {
+      roomCode: "",
+      celebrity: "",
+    },
+  });
+
+  const onSubmit = (data: CreateGameForm) => {
+    console.log("Create Game:", data);
+
+    // Later:
+    // POST /games
+    // Then navigate("/game")
+
+    reset();
+  };
+
   return (
-    <IonPage>  //is one screen of the app*
-      <IonHeader> //top navigation bar 
+    <IonPage>
+      <IonHeader>
         <IonToolbar>
           <IonTitle>Celebrity Name Chain</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen> //where the user interacts 
-        <p>Hello, world!</p>
+
+      <IonContent className="ion-padding">
+
+        <h2>Create New Game</h2>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+
+          <Controller
+            name="roomCode"
+            control={control}
+            render={({ field }) => (
+              <IonItem>
+                <IonLabel position="stacked">
+                  Room Code
+                </IonLabel>
+
+                <IonInput
+                  value={field.value}
+                  placeholder="STAR01"
+                  onIonInput={(e) =>
+                    field.onChange(e.detail.value!)
+                  }
+                />
+              </IonItem>
+            )}
+          />
+
+          <Controller
+            name="celebrity"
+            control={control}
+            render={({ field }) => (
+              <IonItem>
+                <IonLabel position="stacked">
+                  Starting Celebrity
+                </IonLabel>
+
+                <IonInput
+                  value={field.value}
+                  placeholder="Albert Einstein"
+                  onIonInput={(e) =>
+                    field.onChange(e.detail.value!)
+                  }
+                />
+              </IonItem>
+            )}
+          />
+
+          <IonButton
+            type="submit"
+            expand="block"
+            className="ion-margin-top"
+          >
+            Start Game
+          </IonButton>
+
+        </form>
+
       </IonContent>
     </IonPage>
   );
