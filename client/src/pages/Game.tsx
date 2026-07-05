1338e4307c482d3d2163a8311a957f30b8f40fee
salioudunn11 <salioudunn11@gmail.com>
2026-07-05 19:50:37 -0400

    finished game.tsx

--- client/src/pages/Game.tsx
+++ client/src/pages/Game.tsx
@@ -1,55 +1,47 @@
+import {
+  IonButton,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonPage,
+  IonInput,
+} from "@ionic/react";
+import { useState } from "react";
 
+const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:3000";
 
-import{
-     IonButton,
-     IonHeader, 
-     IonTitle, 
-     IonToolbar, 
-     IonPage,
-     IonInput,
-     } from '@ionic/react';
-import { useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
-
-const API_URl = import.meta.env.VITE_API_URL ?? 'http/localhost:3000';
 interface Celebrity {
-    roomCode: string;
-    celebrity: string;
+  roomCode: string;
+  celebrity: string;
 }
-     
-     const Game: React.FC = () => { 
-        const [answer, setAnswer] = useState('');
-        const handleSubmit = () => {
-            console.log(answer);
-            setAnswer('')
-        }
 
-        return (
-            
-            <IonPage> 
-            <IonHeader> 
+const Game: React.FC = () => {
+  const [answer, setAnswer] = useState("");
 
-            <IonToolbar>
-             <IonTitle> This is the game</IonTitle> 
-             </IonToolbar> 
+  const handleSubmit = () => {
+    console.log(answer);
+    setAnswer("");
+  };
 
-             </IonHeader> 
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>This is the game</IonTitle>
+        </IonToolbar>
+      </IonHeader>
 
-                <IonInput>
-                    value = {answer}
-                    placeholder = 'Enter an answer'
-                    onIonInput = {(e) => setAnswer (e.detail.value ?? '')}
-                </IonInput>
+      <IonInput
+        value={answer}
+        placeholder="Enter an answer"
+        onIonInput={(e) => setAnswer(e.detail.value ?? "")}
+      />
 
-             <IonButton expand ="block" onclick= {handleSubmit}>
-             play game
-             </IonButton>
+      <IonButton expand="block" onClick={handleSubmit}>
+        Play Game
+      </IonButton>
+    </IonPage>
+  );
+};
 
-             </IonPage> 
-            
-            );
-         } 
-
-
-         export default Game
-            
+export default Game;
