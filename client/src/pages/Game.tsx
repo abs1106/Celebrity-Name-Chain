3f76e226cd4bdc7d8a961fba57cc9685c80d7997
salioudunn11 <salioudunn11@gmail.com>
2026-07-06 09:45:27 -0400

    making the backend

--- client/src/pages/Game.tsx
+++ client/src/pages/Game.tsx
@@ -7,6 +7,8 @@ import {
   IonInput,
 } from "@ionic/react";
 import { useState } from "react";
+import { useMutation } from '@tanstack/react-query';
+
 
 const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:3000";
 
@@ -22,6 +24,15 @@ const Game: React.FC = () => {
     console.log(answer);
     setAnswer("");
   };
+  const mutation = useMutation({
+    onSuccess: (data) => {
+      console.log('Answer sent successfully!', data);
+    },
+    onError: (error) => {
+      console.error('Error sending Answer:', error);
+    }
+  });
+
 
   return (
     <IonPage>
@@ -37,8 +48,10 @@ const Game: React.FC = () => {
         onIonInput={(e) => setAnswer(e.detail.value ?? "")}
       />
 
-      <IonButton expand="block" onClick={handleSubmit}>
+      <IonButton expand="block" onClick={handleSubmit}
+        disabled={mutation.isPending}>
         Play Game
+        {mutation.isPending ? 'Sending...' : 'Sent'}
       </IonButton>
     </IonPage>
   );
